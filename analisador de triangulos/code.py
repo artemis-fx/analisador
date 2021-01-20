5b39d2e830163a00a593d7ef57a6e38b8cd8303f
Rian Íthalo da Costa Linhares <rianitalolinhares@gmail.com>
2021-01-20 12:28:19 -0300

    criar o analisador

---
+++ analisador de triangulos/code.py
@@ -0,0 +1,18 @@
+from math import fabs
+print('\033[32m-=\033[m'*20)
+print(f'\033[34m{"ANALISADOR DE TRIÂNGULOS":^40}\033[m')
+print('\033[32m-=\033[m'*20)
+l1 = float(input('Primeiro Segmento: '))
+l2 = float(input('Segundo Segmento: '))
+l3 = float(input('Terceiro Segmento: '))
+if fabs(l2-l3) < l1 < l2+l3 and fabs(l1-l3) < l2 < l1+l3 and fabs(l1-l2) < l3 < l1+l2 :
+    print('Os segmentos acima podem formar um Triângulo',end=' ')
+    if l1 != l2 and l1 != l3 and l2 != l3:
+        print('Escaleno')
+    if l1 == l2 and l1 == l3 and l2 == l3:
+        print('Equilátero')
+    #elif l1 == l2 or l1 ==l3 or l2 == l3:
+    else:
+        print('Isósceles')
+else:
+    print('Os segmentos acima não podem formar um Triângulo!!')
